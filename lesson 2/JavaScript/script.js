569644aab6b1f8a05f465326ebc9b8456606e1d1
Home <home@MacBook-Air-Home.local>
2023-04-28 00:51:32 +0300

    first commit

---
+++ lesson 2/JavaScript/script.js
@@ -0,0 +1,20 @@
+let userAge = '22'
+
+console.log(userAge);
+
+let userName = 'Nikita'
+
+console.log(userName);
+
+
+let newUser = 'User';
+
+let shopList ='List';
+
+let userStatus = 'Online' ;
+
+let employSalary = '2300' ;
+
+let cursorCoordinates = `let  cursorCoordinates = 23, 343, 545,`;
+
+console.log(cursorCoordinates);
