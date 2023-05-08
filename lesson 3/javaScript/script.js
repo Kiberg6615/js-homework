7b20ac39c5602c0b46745d886cd8eabe5e0ade5c
nikita9990 <114940429+Kiberg6615@users.noreply.github.com>
2023-05-09 02:29:17 +0300

    Add files via upload

---
+++ lesson 3/javaScript/script.js
@@ -0,0 +1,104 @@
+// Задание 2
+
+// const a = 5;
+// console.log('переменная a =', a);
+// const b = 10;
+// console.log('переменная b =', b);
+
+// const result1 = a + b;
+// console.log('a + b =', result1)
+
+// const result2 = a / b;
+// console.log('a / b =', result2)
+
+// const result3 = a * b;
+// console.log('a * b =', result3)
+
+// const result4 = a - b;
+// console.log('a * b =', result4)
+
+// ---------------------------------
+
+// Задание 3
+
+//  const firstNumb = Number(prompt('Введите первое число '));
+//  const secondNumb = Number(prompt('Введите второе число '));
+
+//  if (firstNumb > secondNumb) {
+//     alert ('Первое число больше второго')
+//  } 
+ 
+//  if (firstNumb < secondNumb) {
+//     alert ('Второе число больше первого')  
+//  }
+
+//  if (firstNumb === secondNumb){
+//     alert ('Числа равны')
+//  }
+
+//-----------------------------------
+
+//Задание 4
+
+
+// const firstNumber =  Number(prompt('Введите первое число  '));
+
+// const nextAction =   String(prompt('Введите желаемое действие (+ - * /) '));
+
+// const secondNumb =   Number(prompt('Введите второе число  '));
+
+ 
+// switch (firstNumber, secondNumb, nextAction ){
+//  case '+' :
+//     alert ( firstNumber + secondNumb );
+//  break;
+
+//  case '-' :
+//     alert ( firstNumber - secondNumb );
+//  break;
+
+//  case '*' :
+//     alert ( firstNumber * secondNumb );
+//  break;
+
+//  case '/' :
+//     alert ( firstNumber / secondNumb );
+//  break;
+// }
+
+//Задание 5 
+
+
+
+// const clientsAge = Number(prompt('Для входа на атракцион уточните пожалуйста ваш возраст?  '));
+ 
+// const minChildAge = 12 ;
+
+// const maxChildAge = 18 ;
+
+// const minPensAge = 60;
+
+// const maxPensAge = 80;
+
+
+ 
+
+//   if (clientsAge > maxChildAge && clientsAge < minPensAge  ){
+//     alert ('Добро пожаловать! ');
+// } 
+
+// else  if (clientsAge < minChildAge || clientsAge > maxPensAge   ){
+//     alert (' Детям и очень пожилым людям вход запрещен!')
+//   } 
+
+// else if((clientsAge >= minChildAge) && (clientsAge <= maxChildAge) || (clientsAge >= minPensAge && clientsAge <= maxPensAge  ), confirm('Есть разрешение взрослых?') )  {
+//     alert('Добро пожаловать!')
+// }  
+//  else {
+//     alert ('Без разрешения взрослых вход запрещён !');
+// }   
+
+// Над последним  заданием сидел 4 часа )
+ 
+
+
