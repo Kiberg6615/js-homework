1d7b971f3d19b5eb9d4dc4aac885696c4e46652e
nikita9990 <114940429+Kiberg6615@users.noreply.github.com>
2023-06-19 17:53:09 +0300

    Add files via upload

---
+++ lesson 4/js/script.js
@@ -0,0 +1,105 @@
+// // Exercise 1
+
+// function showСompare(a, b){
+//   if (a === b){
+//     alert ('True')
+//   }else{
+//     alert ('False')
+//   }
+  
+//}
+
+//____________________________ 
+ 
+   
+//Exercise 2
+
+// function getSum(a = '', b = 'No symbol') {
+//     return a + b;
+// }
+
+// const sumResult = getSum (6,5 )
+//  console.log('Result = ', sumResult);
+
+// const sum =(a, b) => a + b;
+
+// const result = sum(23, 54)
+
+// console.log('FirstSum =',result)
+
+//  const resultSecond = sum(24,555);
+
+//  console.log('SecondResult =', resultSecond)
+
+//Exercise 3
+
+// function getExponentiation(number = 2, degree = '' ){
+    
+//      if (!degree == 0) {
+        
+//      return   number ** degree
+ 
+//      } else {
+//      return   number 
+//      }
+// }
+
+// const exponentiationResult = getExponentiation(2, 3);
+
+// console.log('Result Exponentiation =', exponentiationResult);
+
+ 
+// Exercise 4
+
+// function getDayOfTheWeek(a) {
+    
+//      switch (a){
+//         case 1 :
+//         alert('Ponedilok')
+//         break;
+        
+//         case 2 :
+//         alert('Vtornik')
+//         break;
+
+//         case 3 :
+//         alert('Sreda')
+//         break;
+
+//         case 4 :
+//         alert('Chetverg')
+//         break;
+
+//         case 4 :
+//         alert('Chetverg')
+//         break;
+
+//         case 5 :
+//         alert('Pyatnica')
+//         break;
+
+//         case 6 :
+//         alert('Subbota')
+//         break;
+
+//         case 7 :
+//         alert('Voskresene')
+//         break;
+//      }
+        
+    
+ 
+     
+       
+// }
+
+
+
+//  getDayOfTheWeek (5)
+
+//  console.log(getDayOfTheWeek)
+
+ 
+
+
+ 
